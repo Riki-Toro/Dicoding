bb48b58a89acf7a54d3c95742609e3915eadf6ed
Riki-Toro <rikitoro22@gmail.com>
2021-05-31 22:53:30 +0700

    selesai

--- 1.dasar-pemrograman-web/layout-kalkulator/js/script.js
+++ 1.dasar-pemrograman-web/layout-kalkulator/js/script.js
@@ -58,8 +58,18 @@ function samadenganDisplay() {
     } else {
         result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
     }
+
+    const history = {
+        firstNumber: calculator.firstNumber,
+        secondNumber: calculator.displayNumber,
+        operator: calculator.operator,
+        result: result
+    }
+    putHistory(history);
     
     calculator.displayNumber = result;
+
+    renderHistory();
 }
 
 
