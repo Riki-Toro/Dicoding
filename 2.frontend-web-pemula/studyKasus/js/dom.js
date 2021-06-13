07abbbfced12c8734bfa726decb28a6d4cefc244
Riki-Toro <rikitoro22@gmail.com>
2021-06-13 15:13:15 +0700

    Mengunduh Starter Project, Membuat Berkas JavaScript, dan Menampilkan Todo pada Console

---
+++ 2.frontend-web-pemula/studyKasus/js/dom.js
@@ -0,0 +1,7 @@
+function addToDo() {
+    const titleText = document.getElementById('title').value;
+    const tanggal = document.getElementById('date').value;
+
+    console.log('teks ' + titleText);
+    console.log('waktu ' + tanggal);
+}
