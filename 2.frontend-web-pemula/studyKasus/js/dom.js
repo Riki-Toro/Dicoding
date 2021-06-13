65d3be236497b980f475f85a1d1075cd3c779441
Riki-Toro <rikitoro22@gmail.com>
2021-06-13 20:20:26 +0700

    selesai list todo

--- 2.frontend-web-pemula/studyKasus/js/dom.js
+++ 2.frontend-web-pemula/studyKasus/js/dom.js
@@ -15,7 +15,7 @@ function addToDo() {
     uncompletedTODOList.append(todo);
 }
 
-function makeToDo(data, timesTamp) {
+function makeToDo(data, timesTamp, isCompleted) {
     const textTitle = document.createElement('h2');
     textTitle.innerText = data;
 
@@ -29,7 +29,13 @@ function makeToDo(data, timesTamp) {
     const container = document.createElement('div');
     container.classList.add('item', 'shadow');
     container.append(textContainer);
-    container.append(createCheckButton());
+    // container.append(createCheckButton());
+
+    if(isCompleted) {
+        container.append(createUndoButton(), createTrashButton());
+    } else {
+        container.append(createCheckButton());
+    }
 
     return container;
 }
@@ -45,6 +51,13 @@ function createButton(buttonTypeClass, eventListener) {
 }
 
 function addTaskToCompleted(taskElement) {
+    const taskTitle = taskElement.querySelector('.inner > h2').innerText;
+    const taskTimesTamp = taskElement.querySelector('.inner > p').innerText;
+
+    const newTodo = makeToDo(taskTitle, taskTimesTamp, true);
+    const listCompleted = document.getElementById(COMPLETED_LIST_TODO_ID);
+    listCompleted.append(newTodo);
+
     taskElement.remove();
 }
 
@@ -52,4 +65,29 @@ function createCheckButton() {
     return createButton("check-button", function(event) {
         addTaskToCompleted(event.target.parentElement);
     });
+}
+
+function removeTaskFromCompleted(taskElement) {
+    taskElement.remove();
+}
+function createTrashButton() {
+    return createButton("trash-button", function(event) {
+        removeTaskFromCompleted(event.target.parentElement);
+    });
+}
+
+function undoTaskFromCompleted(taskElement) {
+    const listUncompleted = document.getElementById(UNCOMPLETED_LIST_TODO_ID);
+    const taskTitle = taskElement.querySelector('.inner > h2').innerText;
+    const taskTimesTamp = taskElement.querySelector('.inner > p').innerText;
+
+    const newTodo = makeToDo(taskTitle, taskTimesTamp, false);
+
+    listUncompleted.append(newTodo);
+    taskElement.remove();
+}
+function createUndoButton() {
+    return createButton("undo-button", function(event) {
+        undoTaskFromCompleted(event.target.parentElement);
+    })
 }
