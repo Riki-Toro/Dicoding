61a5587632d0f575d770b659da1f5babf32d62f0
Riki-Toro <rikitoro22@gmail.com>
2021-07-19 14:01:07 +0700

    menambahkan fungsi cekStorage

--- 2.frontend-web-pemula/ta-bookshelfApps/js/data.js
+++ 2.frontend-web-pemula/ta-bookshelfApps/js/data.js
@@ -2,3 +2,11 @@ const STORAGE_KEY = 'BOOK_APPS';
 
 let books = [];
 
+function cekStorage() {
+    if( typeof(Storage) === "undefined" ) {
+        alert('browser anda tidak mendukung web storage!');
+        return false;
+    }
+    return true;
+}
+console.log(cekStorage());
