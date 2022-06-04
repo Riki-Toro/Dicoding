b06edb7b76eacd5fb0546bb208808f4915a6241a
Riki Widiantoro <rikitoro22@gmail.com>
2022-06-04 14:35:51 +0700

    destructuring array dan map

--- 4.dasar-javascript/2.struktur data/destructuring.js
+++ 4.dasar-javascript/2.struktur data/destructuring.js
@@ -11,6 +11,7 @@ console.log(namaDepan, namaBelakang, umur);
 console.log('');
 
 
+
 // destructuring assignment ({code destructuring object})
 const makanann = {
     nama: 'soto',
@@ -26,6 +27,8 @@ let harga = 1000;
 console.log(nama, harga);
 console.log('');
 
+
+
 // default values -> nilai undefined jika tidak diberi nilai default
 const obj = {
     namee: 'Kaneki',
@@ -38,6 +41,7 @@ console.log(namee, namae, umuru, kawin);
 console.log('');
 
 
+
 // Assigning to Different Local Variable Names
 const objj = {
     nameee: 'Kaneki',
@@ -46,15 +50,88 @@ const objj = {
 };
 
 const {nameee: localNamaDepan, namaee: localBelakang, umuruu: localUmur} = objj;
-console.log(localNamaDepan, localBelakang , localUmur);
+console.log(localNamaDepan, localBelakang , localUmur); // harus ada kata local diikuti nama nya
 
 console.log('----------------------------destructuring object----------------------------');
 console.log('');
+console.log('');
+
+
+
+
 
 // destructuring array
+const favorite = ['Seafood', 'Salad', 'Nugget', 'Soup'];
+
+const[firstFood, secondFood, thirdFood, fourthFood] = favorite;
+
+console.log(firstFood);
+console.log(secondFood);
+console.log(thirdFood);
+console.log(fourthFood);
+console.log('');
 
 
+// memilih nilai pada index tertentu untuk destrukturisasi pada array
+const anime = ['snk', 'jujutsu', 'one piece', 'bnh'];
 
+const[ , ,ketiga ] = anime;
+console.log(ketiga);
+console.log('');
+
+
+// Destructuring Assignment
+let myFood = 'Ice Cream';
+let herFood = 'Noodles';
+
+[myFood, herFood] = favorite;
+console.log(myFood);
+console.log(herFood);
+console.log('');
+
+
+// sebelum ada es6
+var a = 1;
+var b = 2;
+var temp;
+
+console.log("sebelum ada es6");
+console.log("Sebelum swap");
+console.log("Nilai a: " + a);
+console.log("Nilai b: " + b);
+
+temp = a;
+a = b;
+b = temp;
+
+console.log("Setelah swap");
+console.log("Nilai a: " + a);
+console.log("Nilai b: " + b);
+console.log('');
+
+// setelah ada es6
+let c = 1;
+let d = 2;
+
+console.log("setelah ada es6");
+console.log("Sebelum swap");
+console.log("Nilai a: " + c);
+console.log("Nilai b: " + d);
+
+[c, d] = [d, c]
+
+console.log("Setelah swap");
+console.log("Nilai a: " + c);
+console.log("Nilai b: " + d);
+console.log('');
+
+
+// Default Values
+const fav = ['rujak'];
+const[pertama, kedua = 'mie ayam'] = fav; // memberikan nilai default pada variabel yang tidak dapat terjangkau oleh array, sehingga nilai pada variabel tidak akan menjadi undefined.
+
+console.log(pertama);
+console.log(kedua);
 
 
 console.log('----------------------------destructuring array----------------------------');
---
+++ 4.dasar-javascript/2.struktur data/map.js
@@ -0,0 +1,8 @@
+// map = adalah tipe data yang menyimpan koleksi data dengan format key-value layaknya Object. Yang membedakan adalah Map memperbolehkan key dengan tipe data apa pun, dibandingkan Object yang hanya mengizinkan key bertipe String atau Symbol.
+
+const map = new Map();
+
+
+
+console.log('---------------------------- map ----------------------------');
+console.log('');
